580b363809d5b3c89fc39c21b888d1d34305ed65
EVA <edie.blues@gmail.com>
2019-04-05 17:12:59 +0100

    Adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Learning French Stuffs`,
+        short_name: `French Stuffs`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/france-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
