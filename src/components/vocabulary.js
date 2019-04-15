f7a10ebaa9f97eebcdb8b4f8237e83979bee1fa4
EVA <edie.blues@gmail.com>
2019-04-15 15:50:07 +0100

    adding days 12-15

--- src/components/vocabulary.js
+++ src/components/vocabulary.js
@@ -19,25 +19,25 @@ function Vocabulary() {
             <td>
               <VocabularyName>Memrise</VocabularyName>
             </td>
-            <td>2537</td>
-            <td>213</td>
-            <td>10</td>
+            <td>4381</td>
+            <td>293</td>
+            <td>15</td>
           </tr>
           <tr>
             <td>
               <VocabularyName>Anki</VocabularyName>
             </td>
-            <td>975</td>
-            <td>122</td>
-            <td>10</td>
+            <td>1665</td>
+            <td>211</td>
+            <td>15</td>
           </tr>
           <tr>
             <td>
               <VocabularyName>ClozeMaster</VocabularyName>
             </td>
-            <td>3974</td>
+            <td>7857</td>
             <td>X</td>
-            <td>6</td>
+            <td>11</td>
           </tr>
         </tbody>
       </table>
